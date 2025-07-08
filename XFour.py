75098bc3abaf99c8b402c2e86d6230a780e0d31d
Shimaa Ameen <shemo.adam@2016@gmail.com>
2025-07-08 20:59:59 +0300

    Fix: added hub.html to templates

--- XFour.py
+++ XFour.py
@@ -7,6 +7,17 @@ def home():
     return render_template("hub.html")
 
 # Include any other routes here (like /about, /daily, etc.)
-
+@app.route("/index")
+def index():
+    return render_template("index.html")
+@app.route("/learnpython")
+def learnpython():
+    return render_template("learnpython.html")
+@app.route("/daily")
+def daily():
+    return render_template("daily.html")
+@app.route("/rpgfront")
+def rpgfront():
+    return render_template("rpgfront.html")
 if __name__ == "__main__":
     app.run()
